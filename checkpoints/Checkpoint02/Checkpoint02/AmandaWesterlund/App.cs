a86b39b7f7e0a0be9be9e7b8c36db6f3f9921e89
Amanda Westerlund <Westerlund922@gmail.com>
2018-03-26 21:12:40 +0200

    validering och någon metod

--- checkpoints/Checkpoint02/Checkpoint02/AmandaWesterlund/App.cs
+++ checkpoints/Checkpoint02/Checkpoint02/AmandaWesterlund/App.cs
@@ -13,52 +13,109 @@ namespace Checkpoint02.AmandaWesterlund
         {
             string[] userRooms = GreetUser();
             Room[] roomArray = new Room[userRooms.Length];
+            bool resultMatch = CheckUserInputFormat(userRooms);
+            if (!resultMatch)
+            {
+                FormatErrorMessage();
+            }
+
+            bool lightInputValid = true;
+            //bool resultMatch = true;
+            int countParts = 0;
+
+            /*
+            foreach (var item in userRooms)
+            {
+                //bool result = Regex.IsMatch(item, @"^((\w[a-zåäö]+)(\d+)(on|off))$");
+                bool result = Regex.IsMatch(item.Trim(), @"^(\w[a-zåäöA-ZÅÄÖ]+)\s(\d+(\s|)m2)\s(on|off)$");
+                if (!result)
+                {
+                    resultMatch = false;
+                    //Console.WriteLine("WRONG!");
+                    //Console.WriteLine(item);
+                }
+            }
+            */
+
             for (var i = 0; i < userRooms.Length; i++)
             {
-                var splitRoom = userRooms[i].Replace("m2", "").TrimStart().TrimEnd().Trim().Split(' ');
+                var splitRoom = userRooms[i].Replace("m2", "").Split(' ', StringSplitOptions.RemoveEmptyEntries); //
+                
+                countParts = splitRoom.Length;
+                
                 //Room room = new Room(splitRoom[0], int.Parse(splitRoom[1]));
                 //roomArray[i] = room;
                 //room.PrintRoom();
+                
                 bool lightOn = false;
                 if (splitRoom[2].ToLower() == "on")
                 {
                     lightOn = true;
                 }
-
+                else if (splitRoom[2].ToLower() != "off")
+                {
+                    lightInputValid = false;
+                }
+                
                 roomArray[i] = new Room(splitRoom[0], int.Parse(splitRoom[1]), lightOn);
                 roomArray[i].PrintRoom();
-
             }
+            if (countParts != 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The input must be three parts");
+                Console.ResetColor();
+            }
+
+            if (lightInputValid == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You didn't enter a valid input for the status of the light switch");
+                Console.ResetColor();
+            }
+            /*
+            if (!result)
+            {
+                resultMatch = false;
+                //Console.WriteLine("WRONG!");
+                //Console.WriteLine(item);
+            }
+            */
 
             var maxRoom = 0;//maxArea;
             int maxRoomInt = 0;
             
-            for (var i = 0; i < roomArray.Length; i++)
-            {
-                var roomSize = roomArray[i].Area;
-                if (roomSize > maxRoom)
+                for (var i = 0; i < roomArray.Length; i++)
                 {
-                    maxRoom = roomSize;
-                    maxRoomInt = i;
+                    var roomSize = roomArray[i].Area;
+                    if (roomSize > maxRoom)
+                    {
+                        maxRoom = roomSize;
+                        maxRoomInt = i;
+                    }
                 }
+
+                int numberOfLightsOn = 0;
+                List<string> roomsWithLightOn = new List<string>();
+                for (var i = 0; i < roomArray.Length; i++)
+                {
+                    if (roomArray[i].LampSwitch)
+                    {
+                        numberOfLightsOn++;
+                        roomsWithLightOn.Add(roomArray[i].Name);
+                    }
+                }
+
+            if (countParts == 3 && lightInputValid && resultMatch)
+            {
+                var numberOfRooms = roomArray.Length;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Det största rummet är {roomArray[maxRoomInt].Name} och är {maxRoom} m2");
+                Console.WriteLine($"Bostaden består av {numberOfRooms} rum");
+                Console.WriteLine($"Ljuset är tänt i {String.Join(" och ", roomsWithLightOn)}");
+                Console.ResetColor();
             }
 
-            int numberOfLightsOn = 0;
-            List<string> roomsWithLightOn = new List<string>();
-            for (var i = 0; i < roomArray.Length; i++)
-            {
-                if (roomArray[i].LampSwitch)
-                {
-                    numberOfLightsOn++;
-                    roomsWithLightOn.Add(roomArray[i].Name);
-                }
-            }
-            
-            var numberOfRooms = roomArray.Length;
-            
-            Console.WriteLine($"Det största rummet är {roomArray[maxRoomInt].Name} och är {maxRoom} m2");
-            Console.WriteLine($"Bostaden består av {numberOfRooms} rum");
-            Console.WriteLine($"Ljuset är tänt i {String.Join(" och ", roomsWithLightOn)}");
             //roomArray[1]
             /*
             Room r1 = new Room(userRooms[0]);
@@ -72,39 +129,67 @@ namespace Checkpoint02.AmandaWesterlund
 
         public string[] GreetUser()
         { 
-            //List<string> rooms = new List<string>();
-            Console.WriteLine("Ange namn på tre rum, ange även dess storlek och om lyset är på eller av. Separera rummen med '|' (t ex. Kök 30m2 on|Toa 10m2 off|Vardagsrum 40m2 on): ");
+            Console.WriteLine("Ange namn på tre rum, ange även dess storlek och om lyset är på eller av. \nSeparera rummen med '|' (t ex. Kök 30m2 on|Toa 10m2 off|Vardagsrum 40m2 on): ");
             var userRooms = Console.ReadLine().Split('|');
             var userRoom = userRooms.ToString();
-            //rooms.Add(userRoom);
             return userRooms;
-           // return userRoom;
 
         }
 
-    }
-    public class Room
-    {
-        public string Name { get; set; }
-        public int Area { get; set; }
-        public bool LampSwitch { get; set; }
-        
-        public Room(string name, int area, bool lampSwitch)
+        public bool CheckUserInputFormat(string[] userRooms)
         {
-            Name = name;
-            Area = area;
-            LampSwitch = lampSwitch;
+            bool resultMatch = true;
+            
+            foreach (var item in userRooms)
+            {
+                //bool result = Regex.IsMatch(item, @"^((\w[a-zåäö]+)(\d+)(on|off))$");
+                bool result = Regex.IsMatch(item.Trim(), @"^(\w[a-zåäöA-ZÅÄÖ]+)\s(\d+(\s|)m2)\s(on|off)$");
+                if (!result)
+                {
+                    resultMatch = false;
+                    //Console.WriteLine("WRONG!");
+                    //Console.WriteLine(item);
+                }
+
+                    
+            }
+
+            return resultMatch;
+
         }
 
-        public void PrintRoom()
+        public void FormatErrorMessage()
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"* Rumsnamn 1: {Name}");
-            //Console.WriteLine($"The biggest room is {Name}, with the area {Area}");
-            //Console.WriteLine($"* Rumsnamn 1: {R1}\n* Rumsnamn 2: {R2}\n* Rumsnamn 3: {R3}");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The input was not in the right format");
             Console.ResetColor();
         }
 
+        public class Room
+        {
+            public string Name { get; set; }
+            public int Area { get; set; }
+            public bool LampSwitch { get; set; }
+
+            public Room(string name, int area, bool lampSwitch)
+            {
+                Name = name;
+                Area = area;
+                LampSwitch = lampSwitch;
+            }
+
+            public void PrintRoom()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"* Rumsnamn 1: {Name}");
+                //Console.WriteLine($"The biggest room is {Name}, with the area {Area}");
+                //Console.WriteLine($"* Rumsnamn 1: {R1}\n* Rumsnamn 2: {R2}\n* Rumsnamn 3: {R3}");
+                Console.ResetColor();
+            }
+
+
+        }
 
     }
     
